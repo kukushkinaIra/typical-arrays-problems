7c3a627fa3944923aa4475135dff100d93805a0f
kukushkinaIra <55156673+kukushkinaIra@users.noreply.github.com>
2020-10-02 23:37:12 +0300

    fix: new solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-gh
+exports.min = function min (array) {
+  if ( array===undefined || array.length==0)
+  return 0;
+  let min=array[0];
+  for (let i=1; i<array.length; i++) {
+    if (array[i]<min)
+     min=array[i];
+  }
+  console.log(min);
+  return min;
+}
+
+exports.max = function max (array) {
+  if ( array===undefined || array.length==0)
+  return 0;
+  let max=array[0];
+  for (let i=1; i<array.length; i++) {
+    if (array[i]>max)
+     max=array[i];
+  }
+  return max;
+}
+
+exports.avg = function avg (array) {
+  if ( array===undefined || array.length==0)
+  return 0;
+  let avg=array.reduce(function (a,b) {
+    return a+b;
+  })
+  return avg/array.length;
+}
